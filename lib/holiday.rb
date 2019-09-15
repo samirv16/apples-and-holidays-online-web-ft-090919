44c2005fc019d31915350519fb76ba8fd7eb8fa6
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-15 04:48:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].keys
+  array - holiday_hash[:winter].keys
+  array.vallues
+  
   end
 end
 
