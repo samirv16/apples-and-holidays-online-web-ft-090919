d7e5946aa5f9f8da2cdeb06b5af67a632d556193
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-15 05:32:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |key, value|
-   puts “#{key.capitalize}:”
+   puts “#{key.to_s.capitalize}:”
    value.each do |holiday, supply|
      array = holiday.to_s.split(" ")
       final_holiday = []
