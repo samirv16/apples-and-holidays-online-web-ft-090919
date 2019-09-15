7eee8718d60d42a78ec09f448d16506412c56493
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-15 03:40:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, value|
   if season == :winter
-    [:christmas] << supply && [:new_years] << supply
+    value << supply
     end
   end
 end
